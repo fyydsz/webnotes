c93ab1c10cd71d27d62de67e0382d7c40defc489
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-12-21 02:07:52 +0700

    Add extensive docs and route metadata

---
+++ app/docs/_meta.js
@@ -0,0 +1,23 @@
+const routes = {
+  index: {
+    title: "Welcome",
+  },
+  tentang: {
+    title: "",
+  },
+  kontribusi: {
+    title: "",
+  },
+  separator: {
+    type: "separator",
+    title: "Courses",
+  },
+  general: {
+    title: "General",
+  },
+  teknik_informatika: {
+    title: "Teknik Informatika",
+  },
+};
+
+export default routes;
