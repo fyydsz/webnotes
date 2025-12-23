60c0d3fc4d685f2a4c43204c5ac088d56d9028d4
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-12-23 14:45:49 +0700

    Add Logika Matematika and update doc hrefs

--- app/docs/program_studi/sistem_informasi/_meta.js
+++ app/docs/program_studi/sistem_informasi/_meta.js
@@ -1,11 +1,15 @@
 const routes = {
   dasar_pemrograman_python: {
     title: "Dasar Pemrograman Python",
-    href: "/docs/program_studi/sistem_informasi/dasar_pemrograman_python",
+    href: "/docs/program_studi/teknik_informatika/dasar_pemrograman_python",
   },
   kalkulus: {
     title: "Kalkulus",
-    href: "/docs/program_studi/sistem_informasi/kalkulus",
+    href: "/docs/program_studi/teknik_informatika/kalkulus",
+  },
+  logika_matematika: {
+    title: "Logika Matematika",
+    href: "/docs/program_studi/teknik_informatika/logika_matematika",
   },
 };
 
